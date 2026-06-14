0abf5ffdfaba19bc79dd2116738948f1130a9485
rex <yu.chenhang@gmail.com>
2026-06-14 23:04:31 +0800

    刪除跟 pack() 衝突的 code

--- src/main/java/tw/com/rex/txt2epub/TxtToEpubApplication.java
+++ src/main/java/tw/com/rex/txt2epub/TxtToEpubApplication.java
@@ -1,21 +1,18 @@
 package tw.com.rex.txt2epub;
 
-import tw.com.rex.txt2epub.panel.MainPanel;
+import java.awt.BorderLayout;
 
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+import tw.com.rex.txt2epub.panel.MainPanel;
 
 public class TxtToEpubApplication {
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-            int height = screenSize.height;
-            int width = screenSize.width;
-
             JFrame frame = new JFrame();
             frame.add(new MainPanel(), BorderLayout.CENTER);
-            frame.setSize(width / 2, height / 2);
             frame.setLocationRelativeTo(null);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setTitle("txt轉EPUB");
